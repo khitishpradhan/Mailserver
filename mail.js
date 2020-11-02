b8678d4c1f22e76aa31686fe45efccb90533e148
Khitish Pradhan <kitzx1999@gmail.com>
2020-11-02 22:51:15 +0530

    first commit

---
+++ mail.js
@@ -0,0 +1,31 @@
+
+const nodemailer = require('nodemailer');
+const mailGun = require('nodemailer-mailgun-transport');
+
+const auth = {
+    auth: {
+        api_key: process.env.API_KEY ||  'MAIL_GUN_API_KEY', 
+        domain: process.env.DOMAIN ||  'MAIL_GUN_DOMAIN'
+    }
+};
+
+const transporter = nodemailer.createTransport(mailGun(auth));
+
+
+const sendMail = (email, subject, text, cb) => {
+    const mailOptions = {
+        from: 'kitzx1999@gmail.com', 
+        to: email, // the receiver email has to be authorized for the free tier
+        subject,
+        text
+    };
+
+    transporter.sendMail(mailOptions, function (err, data) {
+        if (err) {
+            return cb(err, null);
+        }
+        return cb(null, data);
+    });
+}
+
+module.exports = sendMail;
